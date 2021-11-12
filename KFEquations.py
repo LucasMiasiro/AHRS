035b9690b3f534000754e8f0d6c64fc20f9a1824
Lucas Miasiro Ciaramicoli <lucasciaramicoli@hotmail.com>
2021-11-11 21:49:45 -0300

    Fixing bugs on KF

--- KFEquations.py
+++ KFEquations.py
@@ -1,65 +1,38 @@
 from sympy import *
 
-# x, y, z, vx, vy, vz = symbols('x,y,z,v_x,v_y,v_z')
-xp, yp, zp, vxp, vyp, vzp = \
-            symbols('x_{prev},y_{prev},z_{prev},v_{xprev},v_{yprev},v_{zprev}')
+x, v = symbols('x,v')
+a = symbols('a')
+xmeas, vmeas = symbols('x_{meas},v_{meas}')
 dt, g = symbols('dt,g')
-Q1, Q2, Q3, Q4, Q5, Q6 = symbols('Q_1, Q_2, Q_3, Q_4, Q_5, Q_6')
-R1, R2, R3, R4, R5, R6 = symbols('R_1, R_2, R_3, R_4, R_5, R_6')
+Q0, Q1 = symbols('Q_0, Q_1')
+R0, R1 = symbols('R_0, R_1')
 
 # Predict Step
-statep = Matrix([xp, yp, zp, vxp, vyp, vzp])
+statep = Matrix([x, v])
 
 A = Matrix([
-            [1, 0, 0, dt, 0, 0],
-            [0, 1, 0, 0, dt, 0],
-            [0, 0, 1, 0, 0, dt],
-            [0, 0, 0, (dt**2)*g/2, 0, 0],
-            [0, 0, 0, 0, (dt**2)*g/2, 0],
-            [0, 0, 0, 0, 0, (dt**2)*g/2]
-             ])
+            [1, dt],
+            [0, 1]
+            ])
 
-state = A*statep
+B = Matrix([
+            [(dt**2)*g/2],
+            [dt]
+            ])
 
-Pp = MatrixSymbol('Pp', 6, 6)
+statepriori = A*statep + B*a
+
+Pp = MatrixSymbol('Pp', 2, 2)
 
 Q = Matrix([
-            [Q1, 0, 0, 0, 0, 0],
-            [0, Q2, 0, 0, 0, 0],
-            [0, 0, Q3, 0, 0, 0],
-            [0, 0, 0, Q4, 0, 0],
-            [0, 0, 0, 0, Q5, 0],
-            [0, 0, 0, 0, 0, Q6]
-             ])
+            [Q0, 0],
+            [0, Q1]
+            ])
 
 R = Matrix([
-            [R1, 0, 0, 0, 0, 0],
-            [0, R2, 0, 0, 0, 0],
-            [0, 0, R3, 0, 0, 0],
-            [0, 0, 0, R4, 0, 0],
-            [0, 0, 0, 0, R5, 0],
-            [0, 0, 0, 0, 0, R6]
+            [R0, 0],
+            [0, R1]
              ])
 
 Ppriori = A*Matrix(Pp)*transpose(A)
 Ppriori = simplify(Ppriori) + Q
-
-# Update Step
-
-#Assuming H is Identity
-H = Matrix([
-            [1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 1]
-             ])
-
-H0 = transpose(Matrix(H[0:6]))
-K0 = Ppriori*transpose(H0)*(H0*Ppriori*transpose(H0) + R).inv()
-
-
-state
-Ppriori
-# K
--- lib/KF/KF.cpp
+++ lib/KF/KF.cpp
@@ -30,7 +30,7 @@ void KF::correct(){
         _navData->GNSS_ptr->newData = 0;
     }
 
-    states[4] = _navData->IMU_ptr->Home_Alt - *(_navData->B_ptr);
+    states[2] = _navData->IMU_ptr->Home_Alt - *(_navData->B_ptr);
     states[5] = 0;
 }
 
@@ -38,26 +38,26 @@ void KF::fuseGNSS(){
 
     // A posteriori estimate 
     calcKalmanGain(Kx, Px, Rx);
-    calcKalmanGain(Ky, Py, Rz);
+    calcKalmanGain(Ky, Py, Ry);
 
     _navData->GNSS_ptr->getPos(&meas[0], &meas[1]);
 
-    meas[2] = cosf(_navData->GNSS_ptr->GNSS.cog * DEG2RAD)
+    meas[3] = cosf(_navData->GNSS_ptr->GNSS.cog * DEG2RAD)
                     * _navData->GNSS_ptr->GNSS.speed;
-    meas[3] = sinf(_navData->GNSS_ptr->GNSS.cog * DEG2RAD)
+    meas[4] = sinf(_navData->GNSS_ptr->GNSS.cog * DEG2RAD)
                     * _navData->GNSS_ptr->GNSS.speed;
 
     buf_6[0] = meas[0] - states[0];
-    buf_6[1] = meas[1] - states[1];
+    buf_6[1] = meas[3] - states[3];
     matProd_2x2x1(&Kx[0][0], &buf_6[0], &buf_6[0]);
     states[0] += buf_6[0];
-    states[1] += buf_6[1];
+    states[3] += buf_6[1];
 
-    buf_6[2] = meas[2] - states[2];
-    buf_6[3] = meas[3] - states[3];
-    matProd_2x2x1(&Ky[0][0], &buf_6[2], &buf_6[2]);
-    states[2] += buf_6[2];
-    states[3] += buf_6[3];
+    buf_6[0] = meas[1] - states[1];
+    buf_6[1] = meas[4] - states[4];
+    matProd_2x2x1(&Ky[0][0], &buf_6[0], &buf_6[0]);
+    states[1] += buf_6[0];
+    states[4] += buf_6[1];
     
     //A posteriori covariance matrix propagation
     propagateCovarianceMatrixPosteriori(Px, Kx);
@@ -65,10 +65,10 @@ void KF::fuseGNSS(){
 }
 
 void KF::propagateCovarianceMatrixPosteriori(float P[2][2], float K[2][2]){
-    buf_2x2[0][0] = 1 - K[0][0];
+    buf_2x2[0][0] = 1.0 - K[0][0];
     buf_2x2[1][0] = -K[1][0];
     buf_2x2[0][1] = -K[0][1];
-    buf_2x2[1][1] = 1 - K[1][1];
+    buf_2x2[1][1] = 1.0 - K[1][1];
 
     matProd_2x2x2(&buf_2x2[0][0], &P[0][0], &P[0][0]);
 }
@@ -105,13 +105,9 @@ void KF::predict(float *A_E){
 }
 
 void KF::propagateStates(float *A_E){
-    buf_6[3] = states[3];
-    buf_6[4] = states[4];
-    buf_6[5] = states[5];
-
-    states[0] += A_E[1]*hdtsG + buf_6[3]*dt;
-    states[1] += A_E[2]*hdtsG + buf_6[4]*dt;
-    states[2] += A_E[3]*hdtsG + buf_6[5]*dt;
+    states[0] += A_E[1]*hdtsG + states[3]*dt;
+    states[1] += A_E[2]*hdtsG + states[4]*dt;
+    states[2] += A_E[3]*hdtsG + states[5]*dt;
     states[3] += A_E[1]*dt;
     states[4] += A_E[2]*dt;
     states[5] += A_E[3]*dt;
